ce30a90f47b7ea2ad0686c1977c7bcc80f61cb28
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-12-09 21:45:08 +0100

    initial script

---
+++ poor_mans_camera_effects.py
@@ -0,0 +1,14 @@
+import cv2
+
+"""
+1. main
+2. pars args
+   a) list available cams - allow choosing cam that we will be using
+3. passthrough cam to fakecam
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
