a1b846a4ed8fac811bf08abc301e1c783cb7f5f4
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-12-09 22:08:45 +0100

    okay.. opencv doesnt have a method to list cameras.. this or platform specific, directshow api calls for windows needed

--- poor_mans_camera_effects.py
+++ poor_mans_camera_effects.py
@@ -10,7 +10,31 @@ import cv2
 3. passthrough cam to fakecam
 """
 
+def get_available_cameras():
+    idx = 0
+    cameras = []
+    LIMIT_CONSECUTIVE = 3
+    consecutive = 0
+    while consecutive < LIMIT_CONSECUTIVE:
+        capture = cv2.VideoCapture(idx)
+        if not capture.isOpened():
+            consecutive += 1
+            print("{} - fail".format(idx))
+        else:
+            read, img = capture.read()
+            print(read)
+            if read:
+                consecutive = 0
+                cameras.append(idx)
+            capture.release()
+        idx += 1
+    return cameras
+
+
+
 def main():
+    cameras = get_available_cameras()
+    print(cameras)
     pass
 
 if __name__ == '__main__':
